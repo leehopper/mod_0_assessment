091bfa0eb5bdcc87cec80348db92e8fe80c4fcae
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-23 11:45:31 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,19 @@
+# Mod 0 Technical Assessment
+# Part 2: Program Specific Challange
+
+famous_person = "Von Miller"
+
+is_active = true
+
+famous_person = "Peyton Manning"
+
+num_3 = 58
+num_9 = 18
+
+sum = num_3 + num_9
+
+animals = ["Lion", "Tiger", "Monkey"]
+
+animals.shift
+
+puts famous_person.length 
