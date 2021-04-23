e44e3de6a3f8ab23715974b2cc0c25375166607b
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-23 12:10:27 -0600

    Complete modeling using hashes challange b

---
+++ models/model_b.rb
@@ -0,0 +1,14 @@
+# Mod 0 Technical Assessment
+# Part 3: Modeling Using Hashes
+# Question 5, Model of a Pet
+
+pet_dog = {
+  name: "Alfie",
+  breed: "Australian Shepherd",
+  weight_in_pounds: 52,
+  age_in_years: 1.5,
+  fixed: true,
+  vaccinated: true,
+  favorite_toys: ['rope', 'ball', 'kong toy', 'chewy bone'],
+  favorite_games: ['fetch', 'tug of war', 'hide and seek', 'chasing']
+}
