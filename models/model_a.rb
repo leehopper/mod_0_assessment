9af7212a71e5c3509f82f464a85a43d777532919
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-23 12:02:48 -0600

    Complete modeling using hashes challange a

---
+++ models/model_a.rb
@@ -0,0 +1,14 @@
+# Mod 0 Technical Assessment
+# Part 3: Modeling Using Hashes
+# Question 4, Model of a School
+
+school_overview = {
+  "name" => "Lee's School of Excellence",
+  "city" => "Englewood",
+  "number_of_students" => 800,
+  "number_of_teachers" => 80,
+  "in_session" => true,
+  "enrollment_full" => true,
+  "administration_names" => ['Mary', 'Bob', 'Natalie', 'Jerry'],
+  "buildings" => ['Building A', 'Building B', 'Gym', 'Music Hall']
+}
